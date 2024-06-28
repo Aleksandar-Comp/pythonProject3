e76c3a631481f87c5c1fe512d19b1fa40ea3a69c
Aleksandar-Comp <aleksandrgrizoglazov7@gmail.com>
2024-06-28 23:35:54 +0300

    hallo

---
+++ module_3_3.py
@@ -0,0 +1,22 @@
+def print_params (a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+
+def print_params (a, b, c):
+    print(a, b, c)
+
+values_list = [1, 'строка', True]
+values_dict = {'a': 1, 'b': 'строка', 'c': True}
+print_params(*values_list)
+print_params(**values_dict)
+
+def print_params(a, b, c):
+    print(a, b, c)
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
+
+
