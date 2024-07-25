81b80b940f3264ac8857c51d8f57ce830aa544ff
Aleksandar-Comp <aleksandrgrizoglazov7@gmail.com>
2024-07-26 00:38:24 +0300

    hallo

--- module_6_1.py
+++ module_6_1.py
@@ -1,47 +1,65 @@
 class Animal:
-    def __init__(self, name):
-        self.alive = True
-        self.fed = False
+
+    def __init__(self, name, alive: bool = True, fed: bool = False):
         self.name = name
+        self.alive = alive
+        self.fed = fed
 
     def eat(self, food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name} не стал есть {food.name}')
+        if not food.edible:
+            self.fed = False
             self.alive = False
+            print(self.name, 'не стал есть', food.name)
+        else:
+            self.fed = True
+            self.alive = True
+            print(self.name, 'съел', food.name)
+
+
+class Mammal(Animal):
+    def __init__(self, name, alive: bool = True, fed: bool = False):
+        self.name = name
+        self.alive = alive
+        self.fed = fed
+
+
+class Predator(Animal):
+    def __init__(self, name, alive: bool = True, fed: bool = False):
+        self.name = name
+        self.alive = alive
+        self.fed = fed
+
 
 class Plant:
     def __init__(self, name):
-        self.edible = False
         self.name = name
 
-class Mammal(Animal):
-    pass
-
-class Predator(Animal):
-    pass
 
 class Flower(Plant):
-    pass
+    def __init__(self, name, edible: bool = False):
+        self.name = name
+        self.edible = edible
+
 
 class Fruit(Plant):
-    def __init__(self, name):
-        self.edible = True
+    def __init__(self, name, edible: bool = True):
         self.name = name
+        self.edible = edible
 
-a1 = Predator('Волк с Уолл-Стрит')
-a2 = Mammal('Хатико')
-p1 = Flower('Цветик семицветик')
-p2 = Fruit('Заводной апельсин')
 
-print(a1.name)
-print(p1.name)
+if __name__ == '__main__':
 
-print(a1.alive)
-print(a2.fed)
-a1.eat(p1)
-a2.eat(p2)
-print(a1.alive)
-print(a2.fed)
+    a1 = Predator('Волк с Уолл-Стрит')
+    a2 = Mammal('Хатико')
+    p1 = Flower('Цветик семицветик')
+    p2 = Fruit('Заводной апельсин')
+
+    print(a1.name)
+    print(p1.name)
+
+    print(a1.alive)
+    print(a2.fed)
+    a1.eat(p1)
+    a2.eat(p2)
+    print(a1.alive)
+    print(a2.fed)
