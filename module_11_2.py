ac4820fc065859a8a48e67bddbf3080e68673eaf
Aleksandar-Comp <aleksandrgrizoglazov7@gmail.com>
2024-09-22 00:54:02 +0300

    hallo

--- module_11_2.py
+++ module_11_2.py
@@ -1,43 +1,29 @@
+import inspect
 from pprint import pprint
-
-
-class PaginationHelper:
-
-    def __init__(self, collection, items_per_page):
-        self._item_count = len(collection)
-        self.items_per_page = items_per_page
-
-    def item_count(self):
-        return self._item_count
-
-    def page_count(self):
-        return -(self._item_count // -self.items_per_page)
-
-    def page_item_count(self, page_index):
-        return min(self.items_per_page, self._item_count - page_index * self.items_per_page) \
-            if 0 <= page_index < self.page_count() else -1
-
-    def page_index(self, item_index):
-        return item_index // self.items_per_page \
-            if 0 <= item_index < self._item_count else -1
-
-
-helper = PaginationHelper(['a', 'b', 'c', 'd', 'f', 'g'], 4)
-
-helper.page_count()
-helper.item_count()
-print(helper.page_item_count(0))
+from types import ModuleType
+from typing import Dict, Union, Any, List
 
 
 def introspection_info(obj):
-    return_dict = dict()
-    return_dict['type'] = type(obj)
-    return_dict['attributes'] = [i for i in dir(obj)]
-    return_dict['method'] = [getattr(obj, i) for i in dir(obj)
-                             if "<class 'method'>" in str(type(getattr(obj, i)))]
-    return_dict['module'] = helper.__class__
-    return return_dict
+    result: dict[str, Union[Union[str, list[str], ModuleType, None], Any]] = {
+        'type': type(obj),
+        'attribute': [attr for attr in dir(obj) if not callable(getattr(obj, attr))],
+        'methods': [method for method in dir(obj) if callable(getattr(obj, method))],
+        'module': inspect.getmodule(obj),
+        'module_name': introspection_info.__module__,}
+    return result
 
 
-number_info = introspection_info(helper)
-pprint(number_info)
+class Student:
+
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.info()
+
+    def info(self):
+        print(f'My name {self.name}, I am a {self.school} student')
+
+
+number_info1 = introspection_info(42)
+pprint(number_info1)
