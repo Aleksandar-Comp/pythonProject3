d1016645524f925a8fe25cb5f54922eca5967a52
Aleksandar-Comp <aleksandrgrizoglazov7@gmail.com>
2024-10-01 19:02:26 +0300

    hallo

---
+++ module_13_1.py
@@ -0,0 +1,17 @@
+import asyncio
+
+async def start_strongman(name, power):
+    print(f"Силач {name} начал соривнование")
+    for i in range(1, 6):
+        await asyncio.sleep(1 / power)
+    print(f"Силач {name} поднял {i}")
+    print(f"Силач {name} закончил соривнования")
+
+async def start_tournament():
+    participants = [("Pasha", 3), ("Denis", 4), ("Apollon", 5)]
+
+    tasks = [start_strongman(name, power) for name, power in participants]
+    await asyncio.gather(*tasks)
+
+asyncio.run(start_tournament())
+
