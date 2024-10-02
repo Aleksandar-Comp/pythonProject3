e75c22184b1600c605c3e4054de9cab51531ba7c
Aleksandar-Comp <aleksandrgrizoglazov7@gmail.com>
2024-10-02 19:01:08 +0300

    hallo

--- module_13_1.py
+++ module_13_1.py
@@ -2,9 +2,11 @@ import asyncio
 
 async def start_strongman(name, power):
     print(f"Силач {name} начал соривнование")
+
     for i in range(1, 6):
+        print(f"Силач {i} поднял {name}")
         await asyncio.sleep(1 / power)
-    print(f"Силач {name} поднял {i}")
+
     print(f"Силач {name} закончил соривнования")
 
 async def start_tournament():
