d69414295a66d79b956b3af3fd5a359830c55064
Aleksandar-Comp <aleksandrgrizoglazov7@gmail.com>
2024-10-08 18:13:18 +0300

    hallo

--- module_13_2.py
+++ module_13_2.py
@@ -1,15 +1,17 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-api = ""
-bot = Bot(token=api)
-dp = Dispatcher(bot, storage=MemoryStorage())
 
-@dp.message_handler(text=["Хэй!"])
-async def start(message):
-    print("Привет! Я бот помогающий твоему здоровью.")
+api = "8172838769:AAEB3sFRHDh-mpNM7Uc23WmlfM-lTu_dg7k"
+bot = Bot(token=api)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
 @dp.message_handler(commands=["start"])
+async def start_messange(message):
+    print("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message_handler(text="Хэй!")
 async def all_massages(message):
     print("Введите команду /start, чтобы начать общение.")
 
