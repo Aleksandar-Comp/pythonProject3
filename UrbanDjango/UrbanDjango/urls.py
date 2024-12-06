993dc0a69a1a026ef4a9d8fcb2bcf58aaaf26ec9
Aleksandar-Comp <aleksandrgrizoglazov7@gmail.com>
2024-12-07 02:41:44 +0300

    hallo

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 from tempfile import template
 from django.contrib import admin
 from django.urls import path
-from UrbanDjango.task2.views import func_template, ClassTemplates
+#from UrbanDjango.task2.views import func_template, ClassTemplates
+from task3.views import platform, games,cart
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('func/', func_template),
-    path('class/', ClassTemplates.as_view())
+    #path('func/', func_template),
+    #path('class/', ClassTemplates.as_view())
+    path('platform/', platform),
+    path('platform/games/', games),
+    path('platform/cart', cart)
 ]
