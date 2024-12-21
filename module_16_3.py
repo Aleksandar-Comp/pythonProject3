f9b95c994e98bcf28df5657085828bbfdc47aea7
Aleksandar-Comp <aleksandrgrizoglazov7@gmail.com>
2024-12-21 20:13:55 +0300

    hallo

--- module_16_3.py
+++ module_16_3.py
@@ -1,34 +1,39 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path
 from typing import Annotated
 
 app = FastAPI()
 
-users = {"1": "Имя: Example, возраст: 18"}
+users = {'1': 'Имя: Example, возраст: 18'}
 
 
 @app.get("/users")
-async def get_all_users() -> dict:
+async def get_users() -> dict:
     return users
 
 
 @app.post("/user/{username}/{age}")
-async def registration(username: Annotated[str, Path(min_length=3, max_length=16, description="Enter username", example="PashaTehnik")],
-                       age: int = Path(ge=18, le=60, description="Enter age", example="48")) -> str:
-    user_id = str(int(max(users, key=int))+1)
-    users[user_id] = f"Имя: {username}, возраст: {age}"
-    return f"The user {user_id} is registered"
+async def create_user(username: Annotated[str, Path(min_length=5, max_length=30, description="Enter username", example="UrbanUser")],
+                      age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> str:
+
+
+
+    current_id = str(int(max(users, key=int)) + 1)
+    users[current_id] = f"Имя: {username}, возраст: {age}"
+    return f"The user {current_id} is registered"
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: str, username: Annotated[str, Path(min_length=3, max_length=16, description="Enter username", example="PashaTehnik")],
-                      age: int = Path(ge=18, le=60, description="Enter age", example="48")) -> str:
-    if user_id not in users:
-        raise HTTPException(status_code=404, detail="User not found")
-    users[user_id] = f"Имя: {username}, возраст: {age}"
-    return f"User {user_id} has been updated"
+async def update_user(user_id: Annotated[int, Path(ge=0, le=1000000, description="Enter user_id", example="24")],
+                      username: Annotated[str, Path(min_length=5, max_length=30, description="Enter username",
+                                                    example="UrbanUser")], age: Annotated[int, Path(ge=18, le=120,
+                                                        description="Enter age", example="24")]) -> str:
+
+    
+    users[str(user_id)] = f"Имя: {username}, возраст: {age}"
+    return f"The user {user_id} has been updated"
 
 
 @app.delete("/user/{user_id}")
-async def deleted_user(user_id: str) -> str:
-    users.pop(user_id)
-    return f"User {user_id} has been deleted"
+async def delete_user(user_id: Annotated[int, Path(ge=0, le=1000000, description="Enter user_id", example="24")]) -> str:
+    users.pop(str(user_id))
+    return f"The user {user_id} has been deleted"
