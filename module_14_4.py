5a95dbc1fe0e19d0ac8e3911a52ce532da2d77a5
Aleksandar-Comp <aleksandrgrizoglazov7@gmail.com>
2024-12-24 18:36:10 +0300

    hallo

--- module_14_4.py
+++ module_14_4.py
@@ -1,113 +1,45 @@
-from aiogram import Bot, Dispatcher, executor
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from crud_functions import *
-
-from crud_functions import check_and_populate_products, get_all_products, initiate_db
-
-api = ""
-bot = Bot(token=api)
-dp = Dispatcher(bot, storage=MemoryStorage())
-
-kl = InlineKeyboardMarkup(resize_keyboard=True)
-button = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
-button2 = InlineKeyboardButton(text='Формула расчёта', callback_data='formulas')
-kl.add(button)
-kl.add(button2)
+from fastapi import FastAPI, Path, status, Body, HTTPException
+from typing import Annotated, List
+from pydantic import BaseModel
 
 
-kb = InlineKeyboardMarkup(resize_keyboard=True)
-button_ = InlineKeyboardButton(text='Продукт 1', callback_data='product_buying')
-button_2 = InlineKeyboardButton(text='Продукт 2', callback_data='product_buying')
-button_3 = InlineKeyboardButton(text='Продукт 3', callback_data='product_buying')
-button_4 = InlineKeyboardButton(text='Продукт 4', callback_data='product_buying')
-kb.insert(button_)
-kb.insert(button_2)
-kb.insert(button_3)
-kb.insert(button_4)
+app = FastAPI()
 
+users = []
 
-kp = ReplyKeyboardMarkup(resize_keyboard=True)
-button = KeyboardButton(text='Рассчитать')
-button2 = KeyboardButton(text='Информация')
-button3 = KeyboardButton(text='Купить')
-kp.insert(button)
-kp.insert(button2)
-kp.insert(button3)
+class User(BaseModel):
+    id: int = None
+    username: str
+    age: int
 
-initiate_db()
-check_and_populate_products()
-products = get_all_products()
+@app.get('/users')
+def get_users() -> List[User]:
+    return users
 
-@dp.message_handler(text='Купить')
-async def get_buying_list(message):
-    for product in products:
-        id_, title, description, price = product
-        img_path = f'files/{id_}.jpg'
-        with open(f'{img_path}', 'rb') as img:
-            await message.answer_photo(img, caption=f'Название: {title} | Описание: {description} | Цена: {price}p')
-    await message.answer('Выберите продукт для покупки:', reply_markup=kb)
+@app.post('/user/{username}/{age}') # запрос регистрация пользователя
+def create_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                    age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
+    new_id = (users[-1].id + 1) if users else 1
+    new_user = User(id=new_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
 
-@dp.callback_query_handler(text='product_buying')
-async def send_confirm_message(call):
-    await call.message.answer('Вы успешно приобрели продукт!')
-    await call.answer()
+@app.put('/user/{user_id}/{username}/{age}') # запрос на изменение данных пользователя
+def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                      age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    else:
+        raise HTTPException(status_code=404, detail='Пользователя не существует')
 
-@dp.message_handler(text='Рассчитать')
-async def main_menu(message):
-    await message.answer('Выберите опцию:', reply_markup=kl)
-
-@dp.callback_query_handler(text='formulas')
-async def get_formulas(call):
-    await call.message.answer('Для мужчин: 10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5')
-    await call.answer()
-
-
-@dp.message_handler(commands=['start'])
-async def start(message):
-    await message.answer('Привет, я бот помогающий твоему здоровью!', reply_markup=kp)
-#
-@dp.message_handler(text='Информация')
-async def inform(message):
-    await message.answer('Привет, я бот помогающий твоему здоровью!')
-
-
-class UserState(StatesGroup):
-    age = State()
-    growth = State()
-    weight = State()
-
-@dp.callback_query_handler(text='calories')
-async def set_age(call):
-    await call.message.answer('Введите свой возраст:')
-    await UserState.age.set()
-    await call.answer()
-
-@dp.message_handler(state=UserState.age)
-async def set_growth(message, state):
-    await state.update_data(age=int(message.text))
-    await message.answer('Введите свой рост:')
-    await UserState.growth.set()
-
-@dp.message_handler(state=UserState.growth)
-async def set_weight(message, state):
-   await state.update_data(growth=int(message.text))
-   await message.answer('Введите свой вес:')
-   await UserState.weight.set()
-
-@dp.message_handler(state=UserState.weight)
-async def send_calories(message, state):
-    await state.update_data(weight=int(message.text))
-    data = await state.get_data()
-    age = data['age']
-    growth = data['growth']
-    weight = data['weight']
-    calories = 10 * weight + 6.25 * growth - 5 * age + 5
-    await message.answer(f"Ваша дневная норма калорий: {calories} ккал")
-    await state.finish()
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+@app.delete('/user/{user_id}') # запрос на удаление конкретного пользователя
+def delite_user(user_id: int) -> str:
+    for i, user in enumerate(users):
+        if user.id == user_id:
+            return users.pop(i)
+    else:
+        raise HTTPException(status_code=404, detail='Пользователя не существует')
