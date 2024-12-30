976e94a36206505cb310b60aedfda4e89604d2ff
Aleksandar-Comp <aleksandrgrizoglazov7@gmail.com>
2024-12-30 15:01:18 +0300

    hallo

--- UrbanDjango/task1/migrations/initial.py
+++ UrbanDjango/task1/migrations/initial.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 from django.db import migrations, models
 
 
@@ -14,21 +13,21 @@ class Migration(migrations.Migration):
             name='Buyer',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('balance', models.DecimalField(decimal_places=2, default=Decimal('0.00'), max_digits=10)),
-                ('age', models.IntegerField()),
+                ('name', models.CharField(max_length=255)),
+                ('balance', models.DecimalField(decimal_places=2, max_digits=10)),
+                ('age', models.PositiveIntegerField()),
             ],
         ),
         migrations.CreateModel(
             name='Game',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=300)),
+                ('title', models.CharField(max_length=255)),
                 ('cost', models.DecimalField(decimal_places=2, max_digits=10)),
                 ('size', models.DecimalField(decimal_places=2, max_digits=10)),
                 ('description', models.TextField()),
                 ('age_limited', models.BooleanField(default=False)),
-                ('buyer', models.ManyToManyField(related_name='games', to='task1.buyer')),
+                ('buyers', models.ManyToManyField(related_name='games', to='task1.buyer')),
             ],
         ),
     ]
